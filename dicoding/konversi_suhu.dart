d26679c461d1b704923a440eb1fa4bb846ef966e
programmer.nyasar <programmer.nyasar@gmail.com>
2020-06-23 09:34:10 +0700

    penambahan folder dicoding

---
+++ dicoding/konversi_suhu.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+main(){
+
+  stdout.write("Masukan suhu dalam Farenheit : ");
+  var suhu=num.parse(stdin.readLineSync());
+
+  var celcius= (suhu-32)*5/9;
+
+  print("Hasil konversi ke celcius: $celcius");
+
+
+}
