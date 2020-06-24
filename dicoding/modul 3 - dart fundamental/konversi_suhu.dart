c54b6bfb572c2705fe037a0f39d741b6d4df9c04
programmer.nyasar <programmer.nyasar@gmail.com>
2020-06-24 16:11:04 +0700

    penambahan control flow

--- dicoding/modul 3 - dart fundamental/konversi_suhu.dart
+++ dicoding/modul 3 - dart fundamental/konversi_suhu.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+ 
 main(){
 
   stdout.write("Masukan suhu dalam Farenheit : ");
---
+++ dicoding/modul 4 - control flow/do_while.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+
+main(){
+
+  String username;
+  bool notValid = false;
+ 
+do {
+
+  stdout.write('Masukkan nama Anda (min. 6 karakter): ');
+  username = stdin.readLineSync();
+ 
+  if (username.length < 6 ) {
+    notValid = true;
+    print('Username Anda tidak valid');
+  }
+
+  break;
+
+} while (notValid);
+
+
+}
